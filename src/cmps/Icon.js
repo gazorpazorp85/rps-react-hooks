2eedf89346fdda6aa8279da4fff05c3d3cdafa45
gazorpazorp85 <paolo.groppi@gmail.com>
2020-03-12 17:52:05 +0200

    changed hover for big icons

--- src/cmps/Icon.js
+++ src/cmps/Icon.js
@@ -8,7 +8,7 @@ export default function Icon(props) {
     }
 
     return (
-        <div className={`flex center align-center pointer icon-container ${props.icon.name} ${props.icon.size}`}>
+        <div className={`flex center align-center icon-container ${props.icon.name} ${props.icon.size} + ${(props.icon.size) ? '' : 'pointer'} `}>
             <div className="flex center align-center icon-inner-container" onClick={clickHandler}>
                 <img src={`${props.icon.src}`} alt="" />
             </div>
